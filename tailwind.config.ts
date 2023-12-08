8a7681ca34ec71ce2d66d086ee4cf65ca32240ba
Shivam Shahi <shivam.shahi06@gmail.com>
2023-12-08 12:24:20 +0530

    project setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,34 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  plugins: [require("daisyui")],
+  themes: [
+    {
+      mytheme: {
+        primary: "#0059ff",
+
+        secondary: "#00b3ff",
+
+        accent: "#00e900",
+
+        neutral: "#1e0403",
+
+        "base-100": "#182532",
+
+        info: "#007dc7",
+
+        success: "#00de76",
+
+        warning: "#f16100",
+
+        error: "#ff849c",
       },
     },
-  },
-  plugins: [],
-}
-export default config
+  ],
+};
+export default config;
