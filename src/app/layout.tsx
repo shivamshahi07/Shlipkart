5dcb4a9ae46f4f14315b186de71ea93367ed0514
Shivam Shahi <shivam.shahi06@gmail.com>
2023-12-20 18:55:58 +0530

    chore:issue resolved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,10 +19,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <meta property="og:image" content="<generated>" />
-      <meta property="og:image:type" content="<generated>" />
-      <meta property="og:image:width" content="<generated>" />
-      <meta property="og:image:height" content="<generated>" />
       <body className={inter.className}>
         <SessionProvider>
           <Navbar />
